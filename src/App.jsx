f7f4938b7d48a1196f6eb0808423e02b264572a9
AnastasiiaPavliuk <pavliuknst@gmail.com>
2024-03-17 13:56:34 +0100

    implememented function updateData that adds buildings

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Bar from "./components/Bar";
 import ColorSlider from "./components/ColorSlider";
 
 function App() {
-
-  let random;
-  random = Math.floor(Math.random() * 120) + 60;
+  let random = Math.floor(Math.random() * 120) + 60;
 
   const randomize = () => {
     window.location.reload();
@@ -14,15 +12,13 @@ function App() {
     // console.log(random);
   };
 
-   const [color, setColor] = useState(160);
+  const [color, setColor] = useState(160);
 
-   const handleSliderChange = (event) => {
+  const handleSliderChange = (event) => {
     setColor(parseInt(event.target.value)); // Update color with slider value
   };
 
-
-
-  const buildings = [
+  const [buildings, setBuildings] = useState([
     {
       id: 1,
       height: random - 40,
@@ -37,38 +33,45 @@ function App() {
       id: 3,
       height: random - 20,
       windows: true
-    }
-
-    ,{
+    },
+    {
       id: 4,
       height: random - 10,
       windows: true
     }
-  ];
+  ]);
 
   const width = 70;
 
+  const updateData = () => {
+    const lastId = buildings.length > 0 ? buildings[buildings.length - 1].id : 0;
+    const newBuilding = {
+      id: lastId + 1,
+      height: Math.floor(Math.random() * 120) + 60,
+      windows: true // or false depending on your logic
+    };
+    setBuildings([...buildings, newBuilding]);
+  };
 
   return (
     <div className="app">
       <h1>Generate your town!</h1>
       <button onClick={randomize}>randomize</button>
-
-        <ColorSlider color={color} handleSliderChange={handleSliderChange}/>
-
-      <div className="city-cont" style={{ gap: '2px', borderColor: `hsl(${color}, 100%, 50%)`, }}>
-        {buildings.map((building) => (
-          <Bar
-            key={building.id}
-            width={width}
-            height={building.height}
-            color={`hsl(${color}, 70%, 50%)`}
-            // windows={building.windows}
-          />
-        ))}
+      <ColorSlider color={color} handleSliderChange={handleSliderChange}/>
+      <div className="flex-row">
+        <div className="city-cont" style={{ gap: '2px', borderColor: `hsl(${color}, 100%, 50%)`, }}>
+          {buildings.map((building) => (
+            <Bar
+              key={building.id}
+              width={width}
+              height={building.height}
+              color={`hsl(${color}, 70%, 50%)`}
+              // windows={building.windows}
+            />
+          ))}
+        </div>
+        <button onClick={updateData}>+</button>
       </div>
-      <button style={{ height: "40px" }}>Add Building</button>
-
     </div>
   );
 }
